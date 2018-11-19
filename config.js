6292297a8cbd38a55e3fe3ffaf143550b2f84f0d
Choi Jun Su <keroro10398@gmail.com>
2018-11-20 00:54:36 +0900

    init

---
+++ config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  port: 3000,
+  appRoot: __dirname,
+  db: {
+    connectionLimit: 20,
+    host: 'localhost',
+    user: 'root',
+	port: 3306,
+    password: '',
+    database: 'main',
+  },
+};
+
