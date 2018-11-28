9341f1bc4de61bc13dd614cc23bdc7267e54ff68
Choi Jun Su <keroro10398@gmail.com>
2018-11-28 16:52:11 +0900

    db module select 추가 진행중

--- router.js
+++ router.js
@@ -3,12 +3,12 @@ const express = require('express');
 const router = express.Router();
 
 const index = require('./routers/index');
-const users = require('./routers/users');
+const user = require('./routers/user');
 const img = require('./routers/img');
 const upload = require('./routers/upload');
 
 router.use('/', index);
-router.use('/users', users);
+router.use('/user', user);
 router.use('/img', img);
 router.use('/upload', upload);
 
