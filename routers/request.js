460a8215983dbeef548fa0cbb19b765042eb256f
Chloe_Bang <baaaang9753@gmail.com>
2018-12-02 02:41:47 +0900

    request_list (client, freelancer)

--- routers/request.js
+++ routers/request.js
@@ -44,7 +44,12 @@ router.get('/list', wrapper.asyncMiddleware(async (req, res, next) => {
     res.type('html').sendFile(path.join(__dirname, '../public/html/request_list.html'));
 }));
 
-// 의뢰 목록 요청
+
+//~~~~~~~~~~~~질문~~~~~~~~~이 router.get은 freelancer, client 안 만들고 그냥 하나로 쓰는 거 맞지?!?!?
+
+
+
+// 의뢰 목록 요청 _ 관리자
 router.post('/list', wrapper.asyncMiddleware(async (req, res, next) => {
     const queryResult = await db.select({
         from: 'REQUEST',
@@ -53,5 +58,24 @@ router.post('/list', wrapper.asyncMiddleware(async (req, res, next) => {
     res.json(queryResult);
 }));
 
+// 의뢰 목록 요청 _ 프리랜서
+router.post('/freelancer/list', wrapper.asyncMiddleware(async (req, res, next) => {
+    const queryResult = await db.select({
+        from: 'REQUEST',
+        what: ['*'],
+        where: { F_ID :"admin"} //수정필요
+    });
+    res.json(queryResult);
+}));
+
+// 의뢰 목록 요청 _ 의뢰자
+router.post('/client/list', wrapper.asyncMiddleware(async (req, res, next) => {
+    const queryResult = await db.select({
+        from: 'REQUEST',
+        what: ['*'],
+        where : { C_ID : "admin"} //수정 필요
+    });
+    res.json(queryResult);
+}));
 
 module.exports = router;
