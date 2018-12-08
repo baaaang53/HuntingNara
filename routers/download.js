689381070e2438ebda8898593eb3eba37b879ea5
Choi Jun Su <keroro10398@gmail.com>
2018-12-08 23:15:23 +0900

    의뢰 지원자 리스트 및 상세조회

--- routers/download.js
+++ routers/download.js
@@ -6,9 +6,22 @@ const wrapper = require('../modules/wrapper');
 const db = require('../modules/db');
 const zip = require('express-zip');
 
-// 외부 포트폴리오 다운로드 - 프리랜서 id
+// 외부 포트폴리오 다운로드 - session id
 router.get('/outer_portfolio/freelancer', wrapper.asyncMiddleware(async (req, res, next) => {
     const id = req.session.user_id;
+    const queryResult = await db.select({
+        from: 'OUTER_PORTFOLIO',
+        what: ['CONTENT'],
+        where: {
+            F_ID: id
+        }
+    });
+    res.download('.' + queryResult[0]['CONTENT'], queryResult[0]['CONTENT'].split('/')[-1]);
+}));
+
+// 외부 포트폴리오 다운로드 - F_ID
+router.get('/outer_portfolio/client', wrapper.asyncMiddleware(async (req, res, next) => {
+    const id = req.query.fId;
     const queryResult = await db.select({
         from: 'OUTER_PORTFOLIO',
         what: ['CONTENT'],
@@ -31,6 +44,19 @@ router.get('/req_doc', wrapper.asyncMiddleware(async (req, res, next) => {
         }
     });
     res.download('.' + queryResult[i]['FILE'], queryResult[i]['FILE'].split('/')[-1]);
+}));
+
+// 의뢰 완료 리포트 다운로드 - R_NUM
+router.get('/report', wrapper.asyncMiddleware(async (req, res, next) => {
+    const rNum = req.query.rNum;
+    const queryResult = await db.select({
+        from: 'REQUEST',
+        what: ['REPORT'],
+        where: {
+            R_NUM: rNum
+        }
+    });
+    res.download('.' + queryResult[0]['REPORT'], queryResult[0]['REPORT'].split('/')[-1]);
 }));
 
 // 의뢰 완료 리포트 다운로드 - R_NUM
